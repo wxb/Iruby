4dd4cbcde32d250627c51b37162100fb2f792a66
wxb <wxb0328@gmail.com>
2015-10-27 13:42:45 +0800

    wangxb

---
+++ notes/01_Ruby初探/01.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+num = 10
+=begin
+if num > 1 # then
+    print("ok")
+else
+    p("no")
+end
+
+=end
+
+#i = 1
+#while i <= 10
+#    puts "#{i}"
+#    i = i + 1
+#end
+
+100.times do 
+    print('my name is wangxb')
+end
+
